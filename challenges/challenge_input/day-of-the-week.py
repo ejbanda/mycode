a7c5377067f89647fb2c2cab233a7e97640375c5
eric <bandaericj@gmail.com>
2022-11-15 16:04:22 +0000

    lab warmup directory changes

---
+++ challenges/challenge_input/day-of-the-week.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3   
+
+def main():
+
+    user_name = input("Enter your name: ")
+    user_input= input("What day is it today: ")
+
+    print("Hello, " + user_name + "! Happy " + user_input + "!")
+
+main()
+
